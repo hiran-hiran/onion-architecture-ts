9ee5140df423a7cd6e6f501ee50d57a21ac4ffd9
hiran-hiran <nozomu.hirata@timee.co.jp>
2024-08-12 15:02:28 +0900

    feat: valueObject

---
+++ src/Domain/models/shared/ValueObject.ts
@@ -0,0 +1,24 @@
+import { isEqual } from "lodash";
+
+export abstract class ValueObject<T, U> {
+  // @ts-expect-error
+  private _type: U;
+
+  // 継承したclass内でも使える
+  protected _value: T;
+
+  constructor(value: T) {
+    this.validate(value);
+    this._value = value;
+  }
+
+  protected abstract validate(value: T): void;
+
+  get value(): T {
+    return this._value;
+  }
+
+  equals(other: ValueObject<T, U>): boolean {
+    return isEqual(this._value, other._value);
+  }
+}
